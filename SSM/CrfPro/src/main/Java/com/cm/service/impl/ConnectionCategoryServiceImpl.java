493e07c5487036a82ed92f892df968747951c648
ls1rius <865212941@qq.com>
2019-04-25 18:31:22 +0800

    关系处做同上修改

--- SSM/CrfPro/src/main/Java/com/cm/service/impl/ConnectionCategoryServiceImpl.java
+++ SSM/CrfPro/src/main/Java/com/cm/service/impl/ConnectionCategoryServiceImpl.java
@@ -1,10 +1,14 @@
 package com.cm.service.impl;
 
 import com.cm.dao.IConnectionCategoryDao;
+import com.cm.entity.ConnectionCategory;
+import com.cm.entity.LabelCategory;
 import com.cm.service.IConnectionCategoryService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service("connectionCategoryService")
 public class ConnectionCategoryServiceImpl implements IConnectionCategoryService {
@@ -12,7 +16,14 @@ public class ConnectionCategoryServiceImpl implements IConnectionCategoryService
     @Resource
     private IConnectionCategoryDao connectionCategoryDao;
     @Override
-    public Object getConnectionCategory() {
-        return connectionCategoryDao.getConnectionCategory();
+    public List<ConnectionCategory> getConnectionCategory() {
+        ArrayList<ConnectionCategory> connectionCategoryList = (ArrayList<ConnectionCategory>)connectionCategoryDao.getConnectionCategory();
+        int len = (int)connectionCategoryList.size();
+        for(int i =0;i<len;i++){
+            ConnectionCategory tmpCategory = connectionCategoryList.get(i);
+            tmpCategory.setId(i);
+            connectionCategoryList.set(i,tmpCategory);
+        }
+        return connectionCategoryList;
     }
 }
